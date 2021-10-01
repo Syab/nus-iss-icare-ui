4fc30e275dcb4d33c313c846b21a73beedc869d3
syab <syabster.phang18@gmail.com>
2021-10-01 17:32:50 +0800

    changed start command

--- icare-iac/terraform/module/ecs/ecs.tf
+++ icare-iac/terraform/module/ecs/ecs.tf
@@ -9,6 +9,10 @@ resource "aws_ecs_cluster" "icare-cluster-dev" {
   }
 }
 
+//resource "aws_ecs_task_definition" "icare-fe-service" {
+//
+//}
+
 #Outputs for ECS cluster
 output "ecs-icare-cluster-arn" {
   value = aws_ecs_cluster.icare-cluster-dev.arn
